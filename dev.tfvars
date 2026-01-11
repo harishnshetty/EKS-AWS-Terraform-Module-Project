b09d2ff61e712d8c752bbfe1060341118534bcb8
harishnshetty <harishn662@gmail.com>
2026-01-11 10:15:38 +0530

    initial commit

---
+++ dev.tfvars
@@ -0,0 +1,3 @@
+cidr_block              = ["10.0.0.0/16"]
+name                    = "eks-vpc"
+map_public_ip_on_launch = true
---
+++ main.tf
@@ -0,0 +1,8 @@
+module "VPC" {
+  source = "./modules/vpc"
+
+  cidr_block              = var.cidr_block
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+
+  name = var.name
+}
